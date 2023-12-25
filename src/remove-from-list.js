ba2fa3a031cd19ad57807f391e3548d5ad8146dc
Glib Shemenkov <shemenkovglib@gmail.com>
2023-12-25 15:07:52 +0100

    feat: remove

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
 }
 
+const removeKFromList = (l, k) => {
+  let dummy = new ListNode();
+  dummy.next = l;
+  let current = dummy;
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return dummy.next;
+};
+
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
--- test.js
+++ test.js
@@ -1,24 +1,40 @@
-class Queue {
-  constructor() {
-    this.queue = [];
-  }
-  getUnderlyingList() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
-
-  enqueue(value) {
-    this.queue.push(value);
-  }
-
-  dequeue() {
-    return this.queue.shift();
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
   }
 }
 
-const queue = new Queue();
-queue.enqueue(5);
-queue.enqueue(6);
-queue.enqueue(7);
-console.log(queue.dequeue());
-console.log(queue.dequeue());
+const removeKFromList = (l, k) => {
+  let i = 0;
+  let dummy = new ListNode();
+  dummy.next = l;
+  let current = dummy;
+  while (current.next) {
+    console.log(current, i++);
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return dummy.next;
+};
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+const initial = convertArrayToList([3, 1, 2, 3, 4, 5]);
+// console.log(initial);
+// const expected = convertArrayToList([1, 2, 4, 5]);
+// console.log("наш готовый", removeKFromList(initial, 3));
+removeKFromList(initial, 3);
