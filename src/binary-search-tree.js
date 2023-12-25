196c40d98b8486eecb864360ba245d53a0a3f9cf
Glib Shemenkov <shemenkovglib@gmail.com>
2023-12-25 20:16:43 +0100

    feat:tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,146 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
-
+  constructor() {
+    this.roots = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.roots;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.roots) {
+      this.roots = newNode;
+      return;
+    }
+    let currentNode = this.roots;
+    while (currentNode) {
+      if (newNode.data < currentNode.data) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (!this.roots) {
+      return false;
+    }
+    let currentNode = this.roots;
+    while (currentNode) {
+      console.log(currentNode.data);
+      if (currentNode.data === data) {
+        return true;
+      } else {
+        if (currentNode.data > data) {
+          currentNode = currentNode.left;
+        } else currentNode = currentNode.right;
+      }
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (!this.roots) {
+      return null;
+    }
+    let currentNode = this.roots;
+    while (currentNode) {
+      if (currentNode.data === data) {
+        return currentNode;
+      } else {
+        if (currentNode.data > data) {
+          currentNode = currentNode.left;
+        } else currentNode = currentNode.right;
+      }
+    }
+    return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.roots = this.removeNode(this.roots, data);
+  }
+
+  removeNode(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    if (data < node.data) {
+      node.left = this.removeNode(node.left, data);
+    } else if (data > node.data) {
+      node.right = this.removeNode(node.right, data);
+    } else {
+      if (node.left === null && node.right === null) {
+        return null;
+      } else if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+      node.data = this.findMin(node.right).data;
+      node.right = this.removeNode(node.right, node.data);
+    }
+
+    return node;
+  }
+
+  findMin(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.roots) {
+      return null;
+    }
+
+    let currentNode = this.roots;
+    while (currentNode.left) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.roots) {
+      return null;
+    }
+    let currentNode = this.roots;
+    while (currentNode.right) {
+      currentNode = currentNode.right;
+    }
+    return currentNode.data;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
--- test.js
+++ test.js
@@ -1,40 +1,152 @@
-class ListNode {
-  constructor(x) {
-    this.value = x;
-    this.next = null;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
 
-const removeKFromList = (l, k) => {
-  let i = 0;
-  let dummy = new ListNode();
-  dummy.next = l;
-  let current = dummy;
-  while (current.next) {
-    console.log(current, i++);
-    if (current.next.value === k) {
-      current.next = current.next.next;
-    } else {
-      current = current.next;
+class BinarySearchTree {
+  constructor() {
+    this.roots = null;
+  }
+  root() {
+    return this.roots;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.roots) {
+      this.roots = newNode;
+      return;
+    }
+    let currentNode = this.roots;
+    while (currentNode) {
+      if (newNode.data < currentNode.data) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
     }
   }
-  return dummy.next;
-};
 
-function convertArrayToList(arr) {
-  return arr.reverse().reduce((acc, cur) => {
-    if (acc) {
-      const node = new ListNode(cur);
-      node.next = acc;
+  has(data) {
+    if (!this.roots) {
+      return false;
+    }
+    let currentNode = this.roots;
+    let flag = false;
+    while (currentNode) {
+      console.log(currentNode.data);
+      if (currentNode.data === data) {
+        return true;
+      } else {
+        if (currentNode.data > data) {
+          currentNode = currentNode.left;
+        } else currentNode = currentNode.right;
+      }
+    }
+    return flag;
+  }
+
+  find(data) {
+    if (!this.roots) {
+      return null;
+    }
+    let currentNode = this.roots;
+    while (currentNode) {
+      if (currentNode.data === data) {
+        return currentNode;
+      } else {
+        if (currentNode.data > data) {
+          currentNode = currentNode.left;
+        } else currentNode = currentNode.right;
+      }
+    }
+    return null;
+  }
+
+  remove(data) {
+    this.roots = this.removeNode(this.roots, data);
+  }
+
+  removeNode(node, data) {
+    if (!node) {
+      return null;
+    }
+
+    if (data < node.data) {
+      node.left = this.removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this.removeNode(node.right, data);
+      return node;
+    } else {
+      if (!node.left && !node.right) {
+        return null;
+      }
+
+      if (!node.left) {
+        return node.right;
+      }
+
+      if (!node.right) {
+        return node.left;
+      }
+
+      const minRight = this.findMinNode(node.right);
+      node.data = minRight.data;
+      node.right = this.removeNode(node.right, minRight.data);
       return node;
     }
+  }
 
-    return new ListNode(cur);
-  }, null);
+  findMinNode(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  min() {
+    if (!this.roots) {
+      return null;
+    }
+
+    let currentNode = this.roots;
+    while (currentNode.left) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.data;
+  }
+
+  max() {
+    if (!this.roots) {
+      return null;
+    }
+    let currentNode = this.roots;
+    while (currentNode.right) {
+      currentNode = currentNode.right;
+    }
+    return currentNode.data;
+  }
 }
 
-const initial = convertArrayToList([3, 1, 2, 3, 4, 5]);
-// console.log(initial);
-// const expected = convertArrayToList([1, 2, 4, 5]);
-// console.log("наш готовый", removeKFromList(initial, 3));
-removeKFromList(initial, 3);
+const tree = new BinarySearchTree();
+tree.add(2);
+tree.add(1);
+tree.add(3);
+// tree.add(0);
+tree.add(4);
+
+// console.log(tree.root().data);
+console.log(tree.has(0));
+// console.log(tree.max());
